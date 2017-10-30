81e5bb30114c8a68764977d206df60c2943a0948
ChikahoTakayama <puddingstorms@gmail.com>
2017-10-30 17:55:16 +0900

    カーソルの追加 オブジェクトの設置機能追加

---
+++ Assets/CursolController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursolController : MonoBehaviour {
+
+	public GameObject mushroomPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray (Camera.main.transform.position,
+			Camera.main.transform.rotation * Vector3.forward);
+
+		//視線の先に地面があったらサークルを移動
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.transform.gameObject.name == "Platform1") {
+				transform.position = hit.point + new Vector3(0, 0.1f, 0);
+			}
+		}
+
+		//ボタンが押されたらmushroomPrefabを置く
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 pos = transform.position;
+			pos.y = -0.5f;
+			Instantiate (mushroomPrefab, pos, Quaternion.identity);
+		}
+	}
+		
+}
